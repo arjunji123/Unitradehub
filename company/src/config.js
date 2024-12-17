40bad8f0724936d24cbee86c45ba3e34233c87d2
Arjun Singh Naruka <89446608+arjunji123@users.noreply.github.com>
2024-12-17 23:29:42 +0530

    Update config.js

--- company/src/config.js
+++ company/src/config.js
@@ -1,10 +1,10 @@
 
-export const BACKEND_URL = "https://telegram-bot-by30.onrender.com";
+export const BACKEND_URL = "https://admin.theunitradehub.com";
   //export const BACKEND_URL = "http://localhost:4000";
 //   export const FRONTEND_URL =
 //    "https://telegram-bot-git-frontuni-arjuns-projects-e072bddd.vercel.app";
 
 
  export const FRONTEND_URL =
- "https://t.me/TheUnitadeHub_bot";
+ "https://t.me/unitrade_company_bot";
   //"http://localhost:5173"
