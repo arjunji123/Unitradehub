3e8de885eb66f3e3eef95f91cc17564bccfa74e7
Arjun Singh Naruka <89446608+arjunji123@users.noreply.github.com>
2024-12-17 23:16:58 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // src/config.js
-export const BACKEND_URL = "https://admin.theunitradehub.com";
+export const BACKEND_URL = "https://admin.theunitradehub.com/";
   //export const BACKEND_URL = "http://localhost:4000";
 //   export const FRONTEND_URL =
 //    "https://telegram-bot-git-frontuni-arjuns-projects-e072bddd.vercel.app";
