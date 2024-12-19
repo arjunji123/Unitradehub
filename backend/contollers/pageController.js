f765c379eaf737ecc57d7dfd39cdc5503d1fd631
Arjun Singh Naruka <89446608+arjunji123@users.noreply.github.com>
2024-12-19 16:47:14 +0530

    Update pageController.js

--- backend/contollers/pageController.js
+++ backend/contollers/pageController.js
@@ -1235,24 +1235,23 @@ exports.transferPendingCoinsToTotal = catchAsyncErrors(
       //   "INSERT INTO usercoin_audit (user_id, pending_coin, earn_coin) VALUES (?, ?, ?)",
       //   [user_id, updatedPendingCoins, earnCoins]
       // );
-      // await db.query(
-      //   "INSERT INTO usercoin_audit (user_id, pending_coin, earn_coin, title, description, date_entered) VALUES (?, ?, ?, ?, ?, ?)",
-      //   [
-      //     user_id,
-      //     updatedPendingCoins,
-      //     earnCoins,
-      //     title,
-      //     description,
-      //     dateEntered,
-      //   ]
-      // );
-
-         await db.query(
-        "INSERT INTO usercoin_audit (user_id, date_entered) VALUES (?, CURDATE())",
+      await db.query(
+        "INSERT INTO usercoin_audit (user_id, pending_coin, earn_coin, title, description, date_entered) VALUES (?, ?, ?, ?, ?, CURDATE())",
         [
           user_id,
+          updatedPendingCoins,
+          earnCoins,
+          title,
+          description,
         ]
       );
+
+      //    await db.query(
+      //   "INSERT INTO usercoin_audit (user_id, date_entered) VALUES (?, CURDATE())",
+      //   [
+      //     user_id,
+      //   ]
+      // );
 
       // Step 5: Fetch updated values for response
       const updatedPendingCoinsResult = await db.query(
