1f453a28cee0178c9904f659915e82a7a9e9ef6b
Arjun Singh Naruka <89446608+arjunji123@users.noreply.github.com>
2024-12-28 22:32:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use(
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
-app.use(bodyParser.urlencoded({ extended: false })); //For body parser
+app.use(bodyParser.urlencoded({ extended: true })); //For body parser
 app.use(bodyParser.json());
 app.use(expressLayouts);
 app.use(express.static(__dirname + "/public"));
