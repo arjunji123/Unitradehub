76d6e8af5496c0c5d7af21a6c57fa26618afe49f
Arjun Singh Naruka <89446608+arjunji123@users.noreply.github.com>
2025-01-02 15:24:40 +0530

    Update Tasks.jsx

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -586,22 +586,7 @@ function Tasks() {
   </div>
 </div>
 
-{/* CSS for Custom Spinner */}
-<style>
-  .spinner {
-    border: 4px solid #f3f3f3; /* Light background */
-    border-top: 4px solid #000000; /* Black color */
-    border-radius: 50%;
-    width: 20px;
-    height: 20px;
-    animation: spin 1s linear infinite;
-  }
 
-  @keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-  }
-</style>
 
 <Footer />
 
@@ -623,3 +608,21 @@ function Tasks() {
 }
 
 export default Tasks;
+
+
+
+<style>
+  .spinner {
+    border: 4px solid #f3f3f3; /* Light background */
+    border-top: 4px solid #000000; /* Black color */
+    border-radius: 50%;
+    width: 20px;
+    height: 20px;
+    animation: spin 1s linear infinite;
+  }
+
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+</style>
