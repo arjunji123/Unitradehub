d23db7530777ef097bbdf307e490a92032dd679f
Arjun Singh Naruka <89446608+arjunji123@users.noreply.github.com>
2025-01-02 16:18:23 +0530

    Update Tasks.jsx

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -427,22 +427,22 @@ function Tasks() {
         </div>
       </div>
     </div>
-      // }
-      //  <style jsx>{`
-      //   .spinner {
-      //     border: 4px solid #f3f3f3; /* Light background */
-      //     border-top: 4px solid #000000; /* Black color */
-      //     border-radius: 50%;
-      //     width: 20px;
-      //     height: 20px;
-      //     animation: spin 1s linear infinite;
-      //   }
+      )}
+       <style jsx>{`
+        .spinner {
+          border: 4px solid #f3f3f3; /* Light background */
+          border-top: 4px solid #000000; /* Black color */
+          border-radius: 50%;
+          width: 20px;
+          height: 20px;
+          animation: spin 1s linear infinite;
+        }
 
-      //   @keyframes spin {
-      //     0% { transform: rotate(0deg); }
-      //     100% { transform: rotate(360deg); }
-      //   }
-      // `}</style>
+        @keyframes spin {
+          0% { transform: rotate(0deg); }
+          100% { transform: rotate(360deg); }
+        }
+      `}</style>
     <Footer />
   
     {showPopup && (
