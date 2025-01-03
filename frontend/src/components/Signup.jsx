1f59ce3892f96a0d361dc520e9d21a1d7c557463
Arjun Singh Naruka <89446608+arjunji123@users.noreply.github.com>
2025-01-03 16:09:04 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -252,7 +252,7 @@ function Signup() {
    //        <h2 className="text-2xl font-bold text-center text-white mb-6">Create account</h2>
 
    //        <form onSubmit={handleSubmit} className="space-y-4">
-  <div className="bg-black flex justify-center items-center h-screen overflow-hidden">
+<div className="bg-black flex justify-center items-center h-screen overflow-hidden">
   <ToastNotification message={toastMessage} show={showToast} setShow={setShowToast} />
   <div className="w-full max-w-md bg-black text-white rounded-lg shadow-lg font-Inter flex flex-col h-full overflow-hidden">
     <div id="content" className="p-6 space-y-6 flex-grow overflow-y-auto">
