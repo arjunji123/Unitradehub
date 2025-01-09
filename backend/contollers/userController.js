70f2c83375309e8c2706355e8221db7b041cfc17
Arjun Singh Naruka <89446608+arjunji123@users.noreply.github.com>
2025-01-09 18:38:26 +0530

    Update userController.js

--- backend/contollers/userController.js
+++ backend/contollers/userController.js
@@ -897,7 +897,9 @@ Once you've accumulated enough coins, share them with companies at the best rate
 
 👉 Ready to get started? Log in to Unitradehub via Telegram now!
 
+<p>
 🔗 <a href="https://t.me/TheUnitadeHub_bot?startapp=" target="_blank">Click here to access Unitradehub</a>
+</p>
 
 If you have any questions, feel free to contact our support team. We're here to help you every step of the way!
 
